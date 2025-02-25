73b45b59ce5844ed153ef3a1549aa1ed1cb02c42
chris <christopher.liu.0609@gmail.com>
2025-02-25 14:25:23 +0100

    changed directory name

---
+++ snn_model/get_tflite_vela.sh
@@ -0,0 +1,36 @@
+
+
+rm -r saved_models/*
+
+
+
+SAVED_MODELS_DIR="saved_models"
+TF_MODEL_DIR="${SAVED_MODELS_DIR}/TF_model"
+TFLITE_DIR="${SAVED_MODELS_DIR}/tflite_model"
+VELA_TFLITE_DIR="${SAVED_MODELS_DIR}/tflite_vela"
+
+
+mkdir ${TF_MODEL_DIR}
+mkdir ${TFLITE_DIR}
+mkdir ${VELA_TFLITE_DIR}
+
+TFLITE_PATH="${TFLITE_DIR}/tflite_model.tflite"
+
+python3 LIF_model_tf.py ${TF_MODEL_DIR}
+
+
+python3 tf_2_tflite.py ${TF_MODEL_DIR} ${TFLITE_PATH}
+
+
+
+#compile with vela
+vela ${TFLITE_PATH} --output-dir ${VELA_TFLITE_DIR} 
+
+
+echo "Illustrate Vela Compiled Model in Netron? (y/n)"
+
+read userVal
+
+if [ $userVal == "y" ]; then
+	netron saved_models/tflite_vela/tflite_model_vela.tflite
+fi
