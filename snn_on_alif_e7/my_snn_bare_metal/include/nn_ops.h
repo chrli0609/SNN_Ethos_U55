0ab84e238afcbed437862b67d113b47f8402ad19
chris <christopher.liu.0609@gmail.com>
2025-06-10 00:08:12 +0200

    added time measurement

--- snn_on_alif_e7/my_snn_bare_metal/include/nn_ops.h
+++ snn_on_alif_e7/my_snn_bare_metal/include/nn_ops.h
@@ -12,7 +12,8 @@
 
 
 
-#include <ethosu_driver.h>
+//#include <ethosu_driver.h>
+#include "../device/arm_ethos_u55/include/ethosu_driver.h"
 void ethosu_start_pmu_measure(struct ethosu_driver *drv, void *user_arg);
 
 
