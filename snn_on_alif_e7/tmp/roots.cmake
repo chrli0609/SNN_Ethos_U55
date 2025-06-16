2977e98e2b751d218a71a1839cdb5ac48d2d614f
chris <christopher.liu.0609@gmail.com>
2025-06-16 12:16:06 +0200

    Stop tracking file(s) or directory

--- snn_on_alif_e7/tmp/roots.cmake
+++
@@ -1,7 +0,0 @@
-# roots.cmake
-set(CMSIS_PACK_ROOT "C:/Users/chris/AppData/Local/arm/packs" CACHE PATH "CMSIS pack root")
-cmake_path(ABSOLUTE_PATH CMSIS_PACK_ROOT NORMALIZE OUTPUT_VARIABLE CMSIS_PACK_ROOT)
-set(CMSIS_COMPILER_ROOT "C:/Users/chris/.vcpkg/artifacts/2139c4c6/tools.open.cmsis.pack.cmsis.toolbox/2.6.1/etc" CACHE PATH "CMSIS compiler root")
-cmake_path(ABSOLUTE_PATH CMSIS_COMPILER_ROOT NORMALIZE OUTPUT_VARIABLE CMSIS_COMPILER_ROOT)
-set(SOLUTION_ROOT "C:/Users/chris/skolsaker/ak5/mex/project/SNN_Ethos_U55/snn_on_alif_e7" CACHE PATH "CMSIS solution root")
-cmake_path(ABSOLUTE_PATH SOLUTION_ROOT NORMALIZE OUTPUT_VARIABLE SOLUTION_ROOT)
